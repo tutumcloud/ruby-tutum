e84d211522dd17d1c74b1ae2ff746202a7c70774
Mikkel Garcia <mikkel@255bits.com>
2014-09-06 13:54:28 -0600

    [feature] Image API

--- lib/tutum_images.rb
+++ lib/tutum_images.rb
@@ -1,2 +1,43 @@
 class TutumImages < TutumApi
+  def list_url
+    "/image/"
+  end
+
+  def list(params)
+    get(list_url, params)
+  end
+
+  def add_url
+    "/image/"
+  end
+
+  def add(name, params)
+    post(add_url, params.merge({:name => name}))
+  end
+
+  def get_url(name)
+    "/image/#{name}/"
+  end
+
+  def get(name)
+    get(get_url(name))
+  end
+
+  def update_url(name)
+    "/image/#{name}/"
+  end
+  
+  def update(name, params)
+    patch(update_url(name), params)
+  end
+
+  def delete_url(name)
+    "/image/#{name}/"
+  end
+
+  def delete(name)
+    delete(delete_url(name))
+  end
+
+
 end
---
+++ spec/tutum_images_spec.rb
@@ -0,0 +1,22 @@
+require_relative './spec_helper'
+
+describe TutumImages do
+  subject do
+    TutumImages.new({})
+  end
+  it "can list" do
+    expect(subject.list_url()).to eq("/image/")
+  end
+  it "can add" do
+    expect(subject.add_url()).to eq("/image/")
+  end
+  it "can get" do
+    expect(subject.get_url("TEST")).to eq("/image/TEST/")
+  end
+  it "can update" do
+    expect(subject.update_url("TEST")).to eq("/image/TEST/")
+  end
+  it "can delete" do
+    expect(subject.delete_url("TEST")).to eq("/image/TEST/")
+  end
+end
