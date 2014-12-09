49cad7fd8cc7715fd9ef2336d3a501b6ad40e622
Philip (flip) Kromer <flip@infochimps.org>
2014-12-09 03:05:11 -0600

    Catch and release exceptions that are more explainable. Work around problems with ssl certs

--- lib/tutum_api.rb
+++ lib/tutum_api.rb
@@ -18,22 +18,49 @@ class TutumApi
     query =  "?" + params.map { |k,v| "#{k}=#{v}"}.join("&")
     full_path = path
     full_path += query unless params.empty?
-    response = RestClient.get(url(path), headers)
+    response = execute(method: :get, url: url(path), headers: headers, verify_ssl: false)
     JSON.parse(response)
   end
 
   def http_post(path, content={})
-    response = RestClient.post(url(path), content.to_json, headers)
+    response = execute(method: :post, url: url(path), payload: content.to_json, headers: headers, verify_ssl: false)
     JSON.parse(response)
   end
 
   def http_patch(path, content={})
-    response = RestClient.patch(url(path), content.to_json, headers)
+    response = execute(method: :patch, url: url(path), payload: content.to_json, headers: headers, verify_ssl: false)
     JSON.parse(response)
   end
 
   def http_delete(path)
-    response = RestClient.delete(url(path), headers)
+    response = execute(method: :delete, url: url(path), headers: headers, verify_ssl: false)
     JSON.parse(response)
+  end
+
+  # given '/api/v1/service/c4a02992/' or 'c4a02992', returns just the 'c4a02992' part
+  def stripped_id(uuid)
+    uuid.split('/').last
+  end
+
+  class ApiException < ::RestClient::ExceptionWithResponse
+    def initialize(response = nil, initial_response_code = nil, msg = nil)
+      super(response, initial_response_code)
+      @message = msg if msg
+    end
+    def message
+      "#{http_body || 'Unexplained Error'} (#{@message || self.class.name})"
+    end
+    def to_s
+      "#{self.class.name}: #{message}"
+    end
+    def inspect
+      to_s
+    end
+  end
+
+  def execute(query)
+    RestClient::Request.execute(query)
+  rescue RestClient::Exception => err
+    raise ApiException.new(err.response, err.http_code, err.message)
   end
 end
