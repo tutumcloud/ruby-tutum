edbe070129d9f535dab0c0eae449ac773bdc39d3
Matt Solt <mattsolt@gmail.com>
2015-04-28 17:19:39 -0400

    add regions api call and correctly map actions api call (fixes #16 and #17)

--- spec/tutum_spec.rb
+++ spec/tutum_spec.rb
@@ -17,4 +17,36 @@ describe Tutum do
     expect(subject.headers["Authorization"].length).to be > 1
     expect(subject.headers["Accept"]).to eq("application/json")
   end
+
+  it "#actions calls the actions API" do
+    expect(subject.actions).to be_a TutumActions
+  end
+
+  it "#containers calls the containers API" do
+    expect(subject.containers).to be_a TutumContainers
+  end
+
+  it "#node_clusters class the node clusters API" do
+    expect(subject.node_clusters).to be_a TutumNodeClusters
+  end
+
+  it "#node_types calls the node types API" do
+    expect(subject.node_types).to be_a TutumNodeTypes
+  end
+
+  it "#nodes the nodes API" do
+    expect(subject.nodes).to be_a TutumNodes
+  end
+
+  it "#providers calls the providers API" do
+    expect(subject.providers).to be_a TutumProviders
+  end
+
+  it "#regions calls the regions API" do
+    expect(subject.regions).to be_a TutumRegions
+  end
+
+  it "#services services API" do
+    expect(subject.services).to be_a TutumServices
+  end
 end
